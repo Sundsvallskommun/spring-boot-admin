a4fca469ff23f32d5570ab899ee355cd65692d1a
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2022-05-30 16:30:42 +0200

    UF-1993: Fix review comment: Use dept44 1.5, remove @Configuration

--- src/main/java/se/sundsvall/springbootadmin/Application.java
+++ src/main/java/se/sundsvall/springbootadmin/Application.java
@@ -3,14 +3,12 @@ package se.sundsvall.springbootadmin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 
-import org.springframework.context.annotation.Configuration;
 import se.sundsvall.dept44.ServiceApplication;
 import se.sundsvall.dept44.configuration.OpenApiConfiguration;
 import se.sundsvall.dept44.configuration.WebConfiguration;
 
 
 @ServiceApplication(exclude = {WebConfiguration.class, OpenApiConfiguration.class})
-@Configuration
 @EnableAdminServer
 public class Application {
 
