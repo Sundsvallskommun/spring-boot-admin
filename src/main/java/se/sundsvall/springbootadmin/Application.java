30132eb47b4c135d0f43c0b2e9858488f34b00d3
Andreas Carlsson <46994590+foobar-1337@users.noreply.github.com>
2022-10-10 16:19:07 +0200

    Uf 3200 secure app (#11) (#12)
    * UF-3200: Secured pages

--- src/main/java/se/sundsvall/springbootadmin/Application.java
+++ src/main/java/se/sundsvall/springbootadmin/Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.SpringApplication;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import se.sundsvall.dept44.ServiceApplication;
 import se.sundsvall.dept44.configuration.OpenApiConfiguration;
+import se.sundsvall.dept44.configuration.SecurityConfiguration;
 import se.sundsvall.dept44.configuration.WebConfiguration;
 
-@ServiceApplication(exclude = { WebConfiguration.class, OpenApiConfiguration.class })
+@ServiceApplication(exclude = { WebConfiguration.class, OpenApiConfiguration.class, SecurityConfiguration.class })
 @EnableAdminServer
 public class Application {
 	public static void main(String[] args) {
---
+++ src/main/java/se/sundsvall/springbootadmin/configuration/SecurityConfiguration.java
@@ -0,0 +1,78 @@
+package se.sundsvall.springbootadmin.configuration;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.crypto.factory.PasswordEncoderFactories.createDelegatingPasswordEncoder;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+
+/**
+ * The purpose with this class is to configure the paths that should be public accessible.
+ * When (if) this is no longer necessary, this class (and the AdminUser.class) can be removed.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	private static final Duration REMEMBER_ME_DURATION = Duration.ofDays(14);
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http, AdminServerProperties adminServer) throws Exception {
+		http.authorizeRequests(authorizeRequests -> authorizeRequests
+			// Allow these paths for unauthorized users (i.e. public access)
+			.antMatchers(adminServer.path("/login")).permitAll()
+			.antMatchers(adminServer.path("/assets/**")).permitAll()
+			.antMatchers(adminServer.path("/actuator/info")).permitAll()
+			.antMatchers(adminServer.path("/actuator/health/**")).permitAll()
+			.antMatchers(adminServer.path("/wallboard")).permitAll()
+			.antMatchers(adminServer.path("/journal")).permitAll()
+			.antMatchers(GET, adminServer.path("/applications/**")).permitAll()
+			.antMatchers(POST, adminServer.path("/instances")).permitAll()
+			.antMatchers(GET, adminServer.path("/instances/**/details")).permitAll()
+			.antMatchers(GET, adminServer.path("/instances/**/actuator/info")).permitAll()
+			.antMatchers(GET, adminServer.path("/instances/**/actuator/health")).permitAll()
+			.antMatchers(GET, adminServer.path("/instances/**/actuator/metrics/**")).permitAll()
+			.antMatchers(GET, adminServer.path("/instances/events")).permitAll()
+			// All other requests should be protected.
+			.anyRequest().authenticated())
+			// Set up login page. Unauthorized requests will be redirected to the login-page.
+			.formLogin(formLogin -> formLogin.loginPage(adminServer.path("/login")).defaultSuccessUrl(adminServer.path("/wallboard")))
+			.logout(logout -> logout.logoutUrl(adminServer.path("/logout")))
+			// Disables CSRF-Protection for the endpoint the Spring Boot Admin Client uses to (de-)register.
+			.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				.ignoringRequestMatchers(
+					new AntPathRequestMatcher(adminServer.path("/instances"), HttpMethod.POST.toString()),
+					new AntPathRequestMatcher(adminServer.path("/instances/*"), HttpMethod.DELETE.toString()),
+					new AntPathRequestMatcher(adminServer.path("/actuator/**"))))
+			.rememberMe(rememberMe -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds((int) REMEMBER_ME_DURATION.toSeconds()));
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(AdminUser adminUser) {
+		final var passwordEncoder = createDelegatingPasswordEncoder();
+		final var admin = User.withUsername(adminUser.name())
+			.password(adminUser.password())
+			.roles("ADMIN")
+			.passwordEncoder(passwordEncoder::encode)
+			.build();
+
+		return new InMemoryUserDetailsManager(admin);
+	}
+}
