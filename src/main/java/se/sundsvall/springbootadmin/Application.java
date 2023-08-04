259bca8aca0fca46cb54928c480e6907141e48ea
Andreas Carlsson <46994590+foobar-1337@users.noreply.github.com>
2023-08-04 14:48:58 +0200

    Develop (#61)
    * UF-5570: Updated slack certificate (#55)
    
    * UF-5566: Switch to Sonar. (#57)
    
    * UF-5566: Switch to Sonar.
    
    * UF-5566:Switch to Sonar.
    
    * Update README.md
    
    * UF-5575: Refactored permission management. (#60)

--- src/main/java/se/sundsvall/springbootadmin/Application.java
+++ src/main/java/se/sundsvall/springbootadmin/Application.java
@@ -2,18 +2,21 @@ package se.sundsvall.springbootadmin;
 
 import static org.springframework.boot.SpringApplication.run;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import se.sundsvall.dept44.ServiceApplication;
 import se.sundsvall.dept44.configuration.OpenApiConfiguration;
 import se.sundsvall.dept44.configuration.SecurityConfiguration;
 import se.sundsvall.dept44.configuration.WebConfiguration;
+import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedReport;
 
 @ServiceApplication(exclude = { WebConfiguration.class, OpenApiConfiguration.class, SecurityConfiguration.class })
 @EnableAdminServer
 @EnableDiscoveryClient
 @EnableScheduling
+@ExcludeFromJacocoGeneratedReport
 public class Application {
 	public static void main(String... args) {
 		run(Application.class, args);
