ffcb10d1de814340682809225d7267a6ace2663a
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-08-04 13:44:48 +0200

    UF-5575: Refactored permission management.

--- src/main/java/se/sundsvall/springbootadmin/configuration/AdminUser.java
+++ src/main/java/se/sundsvall/springbootadmin/configuration/AdminUser.java
@@ -3,5 +3,4 @@ package se.sundsvall.springbootadmin.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("spring.security.user")
-public record AdminUser(String name, String password) {
-}
+public record AdminUser(String name, String password) {}
